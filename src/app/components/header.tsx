e52df374c0573167d73ee92c3d7f2a4bf5b4b2dc
allanh <allanhy03@yahoo.com>
2024-11-02 16:18:04 -0700

    Merge pull request #1 from allanhy/JIRA-Task-79
    JIRA - 79, 80, 81

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,29 +1,37 @@
+import './header.css'
 import Image from 'next/image';
-
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
+import Link from 'next/link'
+import NavLink from './nav-link'
 
 const Header: React.FC = () => {
+
     return (
-        <nav className='flex items-center justify-between px-6 py-4 mb-5 bg-[#686A6C]'>
-            <div className='flex items-center'>
+        <nav>
+            <div className='flex items-center space-x-8'>
+                <Link href='/home'>
                 <Image src="/TallySight.png"
-                     alt="TallySight Logo"
-                       width={400} // Provide an appropriate width
-                       height={400} // Provide an appropriate height
+                       alt="TallySight Logo"
+                       width={250}
+                       height={100}
                        quality={100}
                        className='m1-16'/>
+                </Link>
+                <ul>
+                    <li><NavLink href='/leaderboards'>Leaderboards</NavLink></li>
+                    <li><NavLink href='/contests'>Contests</NavLink></li>
+                    <li><NavLink href='/quickPicks'>Quick Picks</NavLink></li>
+                </ul>
             </div>
-            <div className='flex items-center text-white'>
+            <div>
                 <SignedOut>
-                    <div className="flex justify-end p-4 rounded-full bg-[#5E6061] hover:scale-105">
+                    <div className='flex justify-end p-3 text-white rounded-lg bg-[#008AFF] hover:scale-105'>
                         <SignInButton />
                     </div>
                 </SignedOut>
                 <SignedIn>
-                    <div className='relative'>
-                        <div className='absolute top-0 right-0 flex justify-end items-start p-4'>
-                            <UserButton />
-                        </div>
+                    <div className='flex justify-end p-1'>
+                        <UserButton />
                     </div>
                 </SignedIn>
             </div>
