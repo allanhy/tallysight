b21db8865914a9a49f46c6c198ff3ab6c84e2edd
Pancho2212 <ahern2212@gmail.com>
2024-11-03 18:51:31 -0800

    Revert "#110, 111"
    This reverts commit 7f6b6efdb118605913cf516c84b4e54090993071.

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,15 +2,21 @@
 as well as go to the leaderboards and MyPicks page
 */}
 
+import Leaderboard from "../components/leaderboard";
+
 export default function Home() {
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black text-white">
+        <div className="grid grid-rows-[20px_1fr_300px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-black text-white">
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
                 <div className="flex flex-col gap-4">
                     <div className="bg-white h-32 w-full" style={{height: '200px ', width: '750px', opacity: 1, transform: 'translateY(100px)'}}/> {/* the most white container */}
                     <div className="bg-gray-200 h-32 w-full" style={{height: '200px ', width: '750px',opacity: 0.75, transform: 'translateY(150px)'}}/> {/* the second most white container */}
                     <div className="bg-gray-400 h-32 w-96" style={{height: '200px ',width: '750px',opacity: 0.5, transform: 'translateY(200px)'}}/>    {/* the third most white container */}
-                    <div className="bg-gray-600 h-32 w-96" style={{height: '200px ',width: '750px', opacity: 0.25, transform: 'translateY(250px)'}}/>   {/* the least most white container */}
+                    <div className="bg-gray-600 h-32 w-96" style={{height: '200px ',width: '750px', opacity: 0.25, transform: 'translateY(250px)'}}/>   
+                    
+                    <div className="relative transform translate-y-56">
+                        <Leaderboard />
+                    </div>
                 </div>
             </main>
         </div>
