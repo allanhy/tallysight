d7a9e0e666d1d320925eecf02b362dcc386702d6
Pancho2212 <ahern2212@gmail.com>
2025-02-23 19:37:34 -0800

    Squashed commit of the following:
    commit 68102926d90b154a8e8727577e7500c9486338a8
    Author: Pancho2212 <ahern2212@gmail.com>
    Date:   Sun Feb 23 19:37:14 2025 -0800
    
        jira-164
    
        fix mjs file to use espn API

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['img.clerk.com'],
+        domains: [
+            'img.clerk.com',
+            'a.espncdn.com',
+            'cdn.nba.com',
+            'cdn.bleacherreport.net',
+            'stats.nba.com',
+            'ak-static.cms.nba.com',
+            'www.nba.com'
+        ],
     },
 };
-
 
 export default nextConfig;
 
