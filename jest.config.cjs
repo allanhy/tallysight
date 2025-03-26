383faa04990a4795e7bdf9f23add007fa65d0782
CSC131Team6 <csc131team@gmail.com>
2025-03-25 17:57:57 -0700

    Squashed commit of the following:
    commit 716a9a44fb25375de479545aa88644fc686194f9
    Author: CSC131Team6 <csc131team@gmail.com>
    Date:   Tue Mar 25 17:57:33 2025 -0700
    
        Update .gitignore
    
    commit 486e2b3ecb57cfc62c22fb0d55d16c605dce5678
    Author: CSC131Team6 <csc131team@gmail.com>
    Date:   Tue Mar 25 17:54:38 2025 -0700
    
        V.2
    
        Fixed the bug that was in socialMediaUpdate
    
    commit 992154f206801cb2e0ab82481bf84c7ca57ce373
    Author: JeremyAuradou <jeremyauradou@gmail.com>
    Date:   Tue Mar 25 17:23:24 2025 -0700
    
        V.1
    
        Started profileUpdate.test.ts and socialMediaUpdate.test.ts, social media test has a bug.

--- jest.config.cjs
+++ jest.config.cjs
@@ -9,4 +9,13 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/$1',
   },
   testMatch: ['**/__tests__/**/*.test.(ts|tsx)'],
+  reporters: [
+    "default",
+    ["./node_modules/jest-html-reporter", {
+      pageTitle: "Test Report: TallySight",
+      includeFailureMsg: true,
+      invludeConsoleLog: true,
+      sort: 'titleAsc'
+    }]
+  ]
 }; 
