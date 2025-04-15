11dec9715af4d0958cebda4df184ea9931775362
Cynthia Angulo <cangulo@csus.edu>
2025-04-15 06:22:09 -0700

    JiraTask229
    I fixed the sizing for MyPicks page and also created a footer with information that includes a page About, Contact Us, Rules,FaQs.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,9 +32,38 @@ export default function RootLayout({
             <SportProvider>
               <div className="flex flex-col min-h-screen">
                 <NavigationWrapper />
-                <main className="flex-1">
+                <main className="flex-1 mb-10">
                   {children}
                 </main>
+                 {/* Global Footer */}
+                 <footer className="bg-gray-100 dark:bg-[#1f2937]text-black dark:text-gray-200 py-10 px-4
+                    border-t border-gray-200 dark:border-gray-700 shadow-md dark:shadow-none transition-colors duration-200">
+                      
+                  <div className="max-w-screen-xl mx-auto">
+                    {/* Header Section */}
+                    <div className="text-center mb-8">
+                      <h2 className="text-4xl sm:text-5xl font-bold mb-2"
+                        style={{ letterSpacing: '1.5px' }}>Let's Play</h2>
+                      
+                      <p className="text-base text-gray-700 dark:text-gray-400">
+                        Make your picks • Win points • Dominate the leaderboard
+                      </p>
+                    </div>
+
+                    {/* Navigation & Info */}
+                    <div className="flex flex-col md:flex-row justify-between items-center">
+                      <div className="flex gap-6 mb-4 md:mb-0">
+                        <a href="/info" className="hover:text-blue-600 dark:hover:text-gray-300 transition-colors duration-200">About</a>
+                        <a href="/info" className="hover:text-blue-600 dark:hover:text-gray-300 transition-colors duration-200">Rules</a>
+                        <a href="/info" className="hover:text-blue-600 dark:hover:text-gray-300 transition-colors duration-200">FAQ</a>
+                        <a href="/info" className="hover:text-blue-600 dark:hover:text-gray-300 transition-colors duration-200">Contact</a>
+                      </div>
+                      <div className="text-sm text-gray-500 dark:text-gray-400">
+                        © {new Date().getFullYear()} Tallysight. All rights reserved.
+                      </div>
+                    </div>
+                  </div>
+                </footer>
               </div>
             </SportProvider>
           </ThemeProvider>
